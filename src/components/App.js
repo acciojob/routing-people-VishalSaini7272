2aadcdb0bed7ad097d8727567338d1b07f89340f
VishalSaini7272 <sainivishal156@gmail.com>
2025-11-06 17:32:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,15 @@
+
 import React from "react";
-import './../styles/App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./UserList";
+import UserDetails from "./UserDetails";
 
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<UserList />} />
+        <Route path="/users/:id" element={<UserDetails />} />
       </Routes>
     </Router>
   );
